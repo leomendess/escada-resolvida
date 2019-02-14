0b4bf8507fb033c1dea696f1627c31a0c5aebe72
Leonardo de Aguiar Mendes <leo_mendesaguiar@hotmail.com>
2019-02-13 22:05:23 -0200

    escada

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log ('exercicio');
+
+let lances = Number(prompt ('quantos lances?'));
+// let material = '#';
+ let material = prompt('qual é o material que você quer utilizar na escada?');
+let degrau = material;
+let i = 0;
+while (i < lances) {
+    console.log(degrau);
+        degrau += material;
+        i++;
+
+
+}
+// let material = prompt('qual é o material que você quer utilizar na escada?');
+
